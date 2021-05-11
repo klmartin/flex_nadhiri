506ad6487117b927eb3b717ce566fcc64d29425a
cits server <server@cits.com>
2021-05-11 08:43:12 -0500

    css & js link modification

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,9 +18,9 @@ scratch. This page gets rid of all links and provides the needed markup only.
    <!-- IonIcons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css')}}">
    <!-- Font Awesome Icons -->
-  <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/plugins/fontawesome-free/css/all.min.css')}}">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+  <link rel="stylesheet" href="{{asset('css/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
 
   <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
   <!-- Theme style -->
